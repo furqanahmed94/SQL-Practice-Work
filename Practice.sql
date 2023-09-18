5e34b3fb3ed9f1d97a11df88dbf7dda657d96808
furqanahmed94 <139892600+furqanahmed94@users.noreply.github.com>
2023-09-18 10:36:46 +0500

    Add files via upload

---
+++ Practice.sql
@@ -0,0 +1,14 @@
+select * from sales
+where amount>2000 and Boxes<100;
+
+select p.Salesperson, count(boxes) as 'Shipment Count'
+from sales s
+join people p on s.spid = p.spid
+where SaleDate between '2022-1-1' and '2022-1-31'
+group by p.Salesperson;
+
+# Which salespersons did not make any shipments in the first 7 days of January 2022?
+select p.salesperson
+from people p
+where p.spid not in
+(select distinct s.spid from sales s where s.SaleDate between '2022-01-01' and '2022-01-07');
